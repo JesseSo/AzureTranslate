a00445873826a79efbd6495701582179cf50f217
J S <soinijesse@gmail.com>
2023-04-14 17:47:54 +0300

    Added text to speech for translation

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import requests, uuid, json
+import requests, json, uuid, os
 from jproperties import Properties
+import azure.cognitiveservices.speech as speechsdk
 
 configs = Properties()
 
@@ -8,19 +9,22 @@ with open('properties', 'rb') as config_file:
 
 constructed_url = "https://api.cognitive.microsofttranslator.com/translate"
 
+language = 'fr'
+
 params = {
     'api-version': '3.0',
     'from': 'en',
-    'to': ['fi']
+    'to': [language]
 }
 
+location = configs.get("location").data
+
 headers = {
-    'Ocp-Apim-Subscription-Key': configs.get("key").data,
-    # location required if you're using a multi-service or regional (not global) resource.
-    'Ocp-Apim-Subscription-Region': configs.get("location").data,
+    'Ocp-Apim-Subscription-Key': configs.get("translation_key").data,
+    'Ocp-Apim-Subscription-Region': location,
     'Content-type': 'application/json',
     'X-ClientTraceId': str(uuid.uuid4())
-}
+    }
 
 textToTranslate = input("Give the text to translate: ")
 
@@ -31,5 +35,25 @@ body = [{
 
 request = requests.post(constructed_url, params=params, headers=headers, json=body)
 response = request.json()
+translations = response[0]['translations']
+text = translations[0]['text']
 
-print(json.dumps(response, sort_keys=True, ensure_ascii=False, indent=4, separators=(',', ': ')))
+#Speech configs
+speech_config = speechsdk.SpeechConfig(subscription=configs.get("speech_key").data, region=location)
+audio_config = speechsdk.audio.AudioOutputConfig(use_default_speaker=True)
+
+# The language of the voice that speaks.
+speech_config.speech_synthesis_voice_name='fr-FR-BrigitteNeural'
+speech_synthesizer = speechsdk.SpeechSynthesizer(speech_config=speech_config, audio_config=audio_config)
+
+speech_synthesis_result = speech_synthesizer.speak_text_async(text).get()
+
+if speech_synthesis_result.reason == speechsdk.ResultReason.SynthesizingAudioCompleted:
+    print("Speech synthesized for text [{}]".format(text))
+elif speech_synthesis_result.reason == speechsdk.ResultReason.Canceled:
+    cancellation_details = speech_synthesis_result.cancellation_details
+    print("Speech synthesis canceled: {}".format(cancellation_details.reason))
+    if cancellation_details.reason == speechsdk.CancellationReason.Error:
+        if cancellation_details.error_details:
+            print("Error details: {}".format(cancellation_details.error_details))
+            print("Did you set the speech resource key and region values?")
